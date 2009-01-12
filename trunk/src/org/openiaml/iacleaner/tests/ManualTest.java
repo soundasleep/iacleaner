482f43302038e4740084326fb0ffa461d6c61659
soundasleep <soundasleep@05b9fe52-e044-11dd-9208-8f743628cd7a>
2009-01-12 02:09:18 +0000

    the manual test script now also writes out the result

--- trunk/src/org/openiaml/iacleaner/tests/ManualTest.java
+++ trunk/src/org/openiaml/iacleaner/tests/ManualTest.java
@@ -4,6 +4,7 @@
 package org.openiaml.iacleaner.tests;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import junit.framework.TestCase;
@@ -27,11 +28,17 @@ import org.openiaml.iacleaner.IACleaner.CleanerException;
 public class ManualTest extends TestCase {
 	
 	public File sourceFile = new File("src/org/openiaml/iacleaner/tests/test.php");
+	public File targetFile = new File("src/org/openiaml/iacleaner/tests/out.php");
 	
 	public void testManual() throws IOException, CleanerException {
 		IACleaner clean = new IACleaner();
 		String output = clean.cleanScript(sourceFile);
 		
 		System.out.println(output);
+		
+		// write it to out.php
+		FileWriter fw = new FileWriter(targetFile);
+		fw.write(output);
+		fw.close();
 	}
 }
