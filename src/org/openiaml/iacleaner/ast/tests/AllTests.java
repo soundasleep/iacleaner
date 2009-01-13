1fdefce1577c283282bcd93d49bd0d5ff6674454
soundasleep <soundasleep@05b9fe52-e044-11dd-9208-8f743628cd7a>
2009-01-13 01:08:10 +0000

    refactoring AST test cases into one adding a complex PHP test file

--- src/org/openiaml/iacleaner/ast/tests/AllTests.java
+++ src/org/openiaml/iacleaner/ast/tests/AllTests.java
@@ -15,18 +15,7 @@ public class AllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for test2.tests");
 		//$JUnit-BEGIN$
-		suite.addTestSuite(FunctionDefinitionTest.class);
-		suite.addTestSuite(CommentsTest.class);
-		suite.addTestSuite(VariableTest.class);
-		suite.addTestSuite(SimpleTest.class);
-		suite.addTestSuite(MultipleLines.class);
-		suite.addTestSuite(SimpleHtmlTest.class);
-		suite.addTestSuite(HtmlTest.class);
-		suite.addTestSuite(ConditionalTest.class);
-		suite.addTestSuite(FunctionComplexTest.class);
-		suite.addTestSuite(FunctionTest.class);
-		suite.addTestSuite(HtmlAttributesTest.class);
-		suite.addTestSuite(ClassTest.class);
+		suite.addTestSuite(LoadingTest.class);
 		//$JUnit-END$
 		return suite;
 	}
