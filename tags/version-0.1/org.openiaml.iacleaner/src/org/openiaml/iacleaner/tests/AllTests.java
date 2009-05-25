26c58ce3297fda3f2db869418a4a4aa612af6f1c
soundasleep <soundasleep@05b9fe52-e044-11dd-9208-8f743628cd7a>
2009-05-25 13:01:58 +0000

    tagging r52 as version 0.1

---
+++ tags/version-0.1/org.openiaml.iacleaner/src/org/openiaml/iacleaner/tests/AllTests.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.openiaml.iacleaner.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Run all automated tests.
+ * 
+ * @author Jevon
+ *
+ */
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TimedTestSuite(
+				"Test for org.openiaml.iacleaner.tests") {
+		};
+		//$JUnit-BEGIN$
+		suite.addTestSuite(DefaultCssTest.class);
+		suite.addTestSuite(PrototypeJsTest.class);
+		suite.addTestSuite(StoreDbPhpTest.class);
+		suite.addTestSuite(ClearSessionPhpTest.class);
+		suite.addTestSuite(IndexTest.class);
+		suite.addTestSuite(ComplexPhpTest.class);
+		suite.addTestSuite(SitemapHtmlTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
