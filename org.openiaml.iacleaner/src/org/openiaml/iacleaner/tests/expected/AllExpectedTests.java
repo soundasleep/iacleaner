ace6166027cb25aa2610149771c81d581f34073c
soundasleep <soundasleep@05b9fe52-e044-11dd-9208-8f743628cd7a>
2009-05-26 13:30:09 +0000

    basic function block indenting in javascript and php

--- org.openiaml.iacleaner/src/org/openiaml/iacleaner/tests/expected/AllExpectedTests.java
+++ org.openiaml.iacleaner/src/org/openiaml/iacleaner/tests/expected/AllExpectedTests.java
@@ -15,16 +15,18 @@ public class AllExpectedTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Expected output tests");
 		//$JUnit-BEGIN$
+		suite.addTestSuite(JsFunctions_Html.class);
+		suite.addTestSuite(Comments_Html.class);
+		suite.addTestSuite(PhpBlock_Php.class);
+		suite.addTestSuite(Strings_Php.class);
+		suite.addTestSuite(Basic_Php.class);
+		suite.addTestSuite(Functions_Php.class);
 		suite.addTestSuite(Attributes_Html.class);
 		suite.addTestSuite(Basic_Html.class);
 		suite.addTestSuite(Inline2_Html.class);
-		suite.addTestSuite(Comments_Html.class);
-		suite.addTestSuite(PhpBlock_Php.class);
 		suite.addTestSuite(JsLanguage_Html.class);
-		suite.addTestSuite(Strings_Php.class);
-		suite.addTestSuite(Inline_Html.class);
-		suite.addTestSuite(Basic_Php.class);
 		suite.addTestSuite(Inline3_Html.class);
+		suite.addTestSuite(Inline_Html.class);
 		suite.addTestSuite(BasicJs_Html.class);
 		//$JUnit-END$
 		return suite;
