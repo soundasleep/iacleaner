5d26378584dbd2e82fbede10ab690e25985f28eb
soundasleep <soundasleep@05b9fe52-e044-11dd-9208-8f743628cd7a>
2009-07-06 10:35:27 +0000

    double quoted strings (\\\') in php and js are now handled successfully

---
+++ org.openiaml.iacleaner/src/org/openiaml/iacleaner/tests/expected/JsStrings_Js.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.openiaml.iacleaner.tests.expected;
+
+/**
+ * @author Jevon
+ *
+ */
+public class JsStrings_Js extends ExpectedTestCase {
+
+	/* (non-Javadoc)
+	 * @see org.openiaml.iacleaner.tests.expected.ExpectedTestCase#getTestCaseClass()
+	 */
+	@Override
+	public Class<?> getTestCaseClass() {
+		return JsStrings_Js.class; 
+	}
+
+}
---
+++ org.openiaml.iacleaner/src/org/openiaml/iacleaner/tests/expected/resources/jsstrings.js
@@ -0,0 +1,11 @@
+
+alert("a");
+
+alert( "a\"" );
+
+alert("a \\");
+
+alert("a \\\" ); echo (") ;
+alert( 'a' );
+alert('a\'');alert('a \\');
+alert( 'a \\\' ); echo (' ) ;
