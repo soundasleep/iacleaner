37575957b2a54458516d278fc904588e9036db1f
soundasleep <soundasleep@05b9fe52-e044-11dd-9208-8f743628cd7a>
2009-11-30 08:03:15 +0000

    fixing a bug where the division symbol '/' would be lost in a following PHP block comment removing dead code warning

--- trunk/org.openiaml.iacleaner/src/org/openiaml/iacleaner/tests/expected/resources/phpoperations.compact.php
+++ trunk/org.openiaml.iacleaner/src/org/openiaml/iacleaner/tests/expected/resources/phpoperations.compact.php
@@ -1,1 +1,1 @@
-<?php $a=null;$a=$a+1;$a=$a-1;$a=$a*$a;$a=$a/$a;$a=$a.$a;$a='hello'.' world';$a="hello"." world";$a='hello$a';$a="hello$a";$a+=$a;$a-=$a;$a*=$a;$a/=$a;$a.=$a;$a=$a&&$b&$c;$a=$a||$b|$c;$a&=$a;$a|=$b;$a=$f^$g;$a^=$g;$a=($b*$c)/(($d*$e)^$f);$a=!!$a;?>
+<?php $a=null;$a=$a+1;$a=$a-1;$a=$a*$a;$a=$a/$a;$a=$a.$a;$a='hello'.' world';$a="hello"." world";$a='hello$a';$a="hello$a";$a+=$a;$a-=$a;$a*=$a;$a/=$a;$a.=$a;$a=$a&&$b&$c;$a=$a||$b|$c;$a&=$a;$a|=$b;$a=$f^$g;$a^=$g;$a=($b*$c)/(($d*$e)^$f);$a=!!$a;$a/* c */=/* c */$a/* c */ / /* c */$a;?>
