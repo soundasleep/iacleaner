011a67bb898b1f12c35f8040331605e03a6510d2
soundasleep <soundasleep@05b9fe52-e044-11dd-9208-8f743628cd7a>
2011-04-07 06:46:18 +0000

    it should not be possible to wordwrap while in the middle of an attribute value in an XML tag

---
+++ org.openiaml.iacleaner/src/org/openiaml/iacleaner/tests/expected/XmlLong_Php.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.openiaml.iacleaner.tests.expected;
+
+/**
+ * @author Jevon
+ *
+ */
+public class XmlLong_Php extends ExpectedTestCase {
+
+	/* (non-Javadoc)
+	 * @see org.openiaml.iacleaner.tests.expected.ExpectedTestCase#getTestCaseClass()
+	 */
+	@Override
+	public Class<?> getTestCaseClass() {
+		return XmlLong_Php.class; 
+	}
+
+}
---
+++ org.openiaml.iacleaner/src/org/openiaml/iacleaner/tests/expected/resources/xmllong.compact.php
@@ -0,0 +1,9 @@
+<?php echo("hello, world! ");if(function());?><?xml version="1.0" ?>
+<root>
+  <node></node>
+  <attribute attr="foo"> with text <?php echo "hello, world!";
+  if (function()) echo "four"; ?></attribute>
+  <long one="one" two="two" three="three" four="four" five="five" six="six" seven="seven" eight="eight" nine="nine" ten="ten" long="this is a really long attribute" another="and very long attributes should not have word wrap applied to them">
+  	This is a big block of text. Because XML encoding can be whitespace-sensitive, we can't guarantee that indenting this line will be successful.
+  </long>
+</root>
